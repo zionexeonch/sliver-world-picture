7b127d607fb3ba6ef7f80f1474dc6cdfa1c88ab6
zionexeonch <zionexeon@gmail.com>
2023-10-09 07:18:24 +0700

    second commit

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -22,6 +22,22 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+        // Anime::create([
+        //     "url_image" => "https://res.cloudinary.com/dnmkw2715/image/upload/v1691572544/zionexeonch/art/Desain_tanpa_judul_14_nfntsj.png",
+        //     "judul" => "alfkjalfjaf",
+        //     "jepang" => "faoeifjald",
+        //     "slug" => ";lfajoeawjf",
+        //     "produser_or_author" => "alfkjalfjaf",
+        //     "tipe" => "tv",
+        //     "total" => "24",
+        //     "durasi" => "24 min",
+        //     "studio" => "mappa",
+        //     "rating" => "9.90",
+        //     "genre" => "yaoi",
+        //     "status" => "ongoing",
+        //     "hari_rilis" => "1999-05-1",
+        //     "deskripsi" => "asdfasfasdfawre",
+        // ]);
         Anime::factory()->count(50)->create();
         Manga::factory()->count(50)->create();
         Movie::factory()->count(50)->create();
